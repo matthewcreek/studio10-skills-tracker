2aeb5764c4839c4b4cea784504a4ad6b101b8ae9
Matthew Creek <matthew.creek27@gmail.com>
2023-02-06 19:05:21 -0600

    initial commit

---
+++ src/main/java/org/launchcode/skilllstracker/SkilllsTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.skilllstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilllsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilllsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/skilllstracker/SkilllsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.skilllstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilllsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
